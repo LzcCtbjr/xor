4dc68cd005849f1bd4ba6cf6447c22668c9e7e02
vidhaat <dunna.vidhaat@gmail.com>
2019-07-10 08:31:24 -0700

    finished work on xorAnalysis.py

--- xorAnalysis.py
+++ xorAnalysis.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from xorTools import *
-import matplotlib.pyplot as plt
+from tabulate import tabulate
 
 w = 25
 h = 25
@@ -10,9 +10,6 @@ for i in range(0, w):
     for j in range(0,h):
         arr[i][j] = decXor(i,j)
 
-table = plt.table(cellText=arr,
-                    rowLabels=arr[0][:],
-                    colLabels=arr[:][0],
-                    loc='top')
-
-plt.show()
+with open('output.txt', 'w') as f:
+    print(tabulate(arr))
+    f.write(tabulate(arr))
