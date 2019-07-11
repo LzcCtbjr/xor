34d1cc62738258fec00243d385a904be531b8572
vidhaat <dunna.vidhaat@gmail.com>
2019-07-11 08:22:01 -0700

    you can now specify the bounds of the table you want to generate

--- xorAnalysis.py
+++ xorAnalysis.py
@@ -2,12 +2,13 @@
 from xorTools import *
 from tabulate import tabulate
 
-w = 25
-h = 25
-arr = [[0 for x in range(w)] for y in range(h)]
+w = int(input('hrange: '))
+h = int(input('vrange: '))
 
-for i in range(0, w):
-    for j in range(0,h):
+arr = [[0 for y in range(w)] for x in range(h)]
+
+for i in range(0, h):
+    for j in range(0,w):
         arr[i][j] = decXor(i,j)
 
 with open('output.txt', 'w') as f:
